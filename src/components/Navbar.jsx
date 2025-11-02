085fd628baceffebe4faee9f2e97754f682e25f9
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2025-11-02 14:51:19 +0600

    navbar done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <div className='flex justify-between'>
+            <div className=""></div>
+            <div className="nav space-x-5">
+                <Link to="/">Home</Link>
+                <Link to="/career">Career</Link>
+                <Link to="/about">About</Link>
+            </div>
+            <div className="login"></div>
+        </div>
+    );
+};
+
+export default Navbar;
