3cd5a4df28b8aac428984021d3d0678bac58f548
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2025-11-02 16:04:00 +0600

    completed the left navbar

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "../components/Header";
 import LatestNews from "../components/LatestNews";
 import Navbar from "../components/Navbar";
+import LeftNavbar from "../components/layout-component/LeftNavbar";
 
 const HomeLayout = () => {
 	return (
@@ -15,7 +16,11 @@ const HomeLayout = () => {
 			<nav className="w-11/12 mx-auto py-2">
 				<Navbar></Navbar>
 			</nav>
-			{/* <Main></Main> */}
+			<main className="w-11/12 mx-auto pt-5 grid md:grid-cols-12 gap-3">
+				<aside className="col-span-3"><LeftNavbar></LeftNavbar></aside>
+                <section className="col-span-6">Main conctent</section>
+                <aside className="col-span-3">Right navbar</aside>
+			</main>
 		</div>
 	);
 };
