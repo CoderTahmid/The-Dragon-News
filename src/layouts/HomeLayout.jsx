d67851c88dfe815861e54918b6b01df5ed847652
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2025-11-02 15:01:41 +0600

    Navbar's login part done

--- src/layouts/HomeLayout.jsx
+++ src/layouts/HomeLayout.jsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import Header from '../components/Header';
-import LatestNews from '../components/LatestNews';
-import Navbar from '../components/Navbar';
+import React from "react";
+import Header from "../components/Header";
+import LatestNews from "../components/LatestNews";
+import Navbar from "../components/Navbar";
 
 const HomeLayout = () => {
-    return (
-        <div>
-            <header>
-                <Header></Header>
-                <section className='w-11/12 mx-auto'>
-                    <LatestNews></LatestNews>
-                </section>
-                <section className='w-11/12 mx-auto py-2'>
-                    <Navbar></Navbar>
-                </section>
-            </header>
-            {/* <Nav></Nav> */}
-            {/* <Main></Main> */}
-        </div>
-    );
+	return (
+		<div>
+			<header>
+				<Header></Header>
+				<section className="w-11/12 mx-auto">
+					<LatestNews></LatestNews>
+				</section>
+			</header>
+			<nav className="w-11/12 mx-auto py-2">
+				<Navbar></Navbar>
+			</nav>
+			{/* <Main></Main> */}
+		</div>
+	);
 };
 
 export default HomeLayout;
