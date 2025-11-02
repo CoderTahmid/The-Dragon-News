cd1179b530710a2f6c1101888d5095823598238a
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2025-11-02 22:17:18 +0600

    made a layout for auth pages

---
+++ src/layouts/AuthLayout.jsx
@@ -0,0 +1,15 @@
+import {Outlet} from "react-router-dom";
+import Navbar from "../components/Navbar";
+
+const AuthLayout = () => {
+	return (
+		<div>
+            <header className="py-3 w-11/12 mx-auto bg-[#F3F3F3]">
+                <Navbar></Navbar>
+            </header>
+			<Outlet></Outlet>
+		</div>
+	);
+};
+
+export default AuthLayout;
