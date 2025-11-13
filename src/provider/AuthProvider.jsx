d001ef91bb8e505333a2292775c5e5791cc611c3
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2025-11-13 12:52:29 +0600

    added an observer, so that the user data do not lost after refresh

--- src/provider/AuthProvider.jsx
+++ src/provider/AuthProvider.jsx
@@ -1,26 +1,34 @@
-import {createContext, useState} from "react";
+import {createContext, useEffect, useState} from "react";
 import app from "../firebase/firebase.config";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import {createUserWithEmailAndPassword, getAuth, onAuthStateChanged} from "firebase/auth";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
 
 const AuthProvider = ({children}) => {
-	const [user, setUser] = useState(null)
+	const [user, setUser] = useState(null);
 
 	const createNewUser = (email, passwrod) => {
 		return createUserWithEmailAndPassword(auth, email, passwrod);
-	}
+	};
 
 	const authInfo = {
 		user,
 		setUser,
-		createNewUser
+		createNewUser,
 	};
 
-	return <AuthContext.Provider value={authInfo}>
-        {children}
-    </AuthContext.Provider>;
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+		})
+
+		return () => {
+			unsubscribe();
+		}
+	}, []);
+
+	return <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
