cfebaa475acdab2cc231e200730dfe8501a5cd25
Tahmid Ibne Mofazzol <tahmid.ibne@gmail.com>
2025-11-14 06:28:09 +0600

    secured the API keys, made a env file

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAqq4zDJP4nP62ydP0KkFkWdf7c26dYKjw",
-  authDomain: "dragon-news-dd4d6.firebaseapp.com",
-  projectId: "dragon-news-dd4d6",
-  storageBucket: "dragon-news-dd4d6.firebasestorage.app",
-  messagingSenderId: "127847807549",
-  appId: "1:127847807549:web:30d468074c6af6cfc6c854"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId
 };
 
 // Initialize Firebase
